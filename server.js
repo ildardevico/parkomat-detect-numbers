c16e7bea665720b518832d30ed70e0602c00031d
Ildar <ildarka008@gmail.com>
2018-11-24 06:09:32 +0200

    feature(webcam): add ability to make photo from webcam and parse it

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 const express = require('express');
+const webcam = require('node-webcam');
 const app = express();
-const multipart = require('connect-multiparty');
 const cloudinary = require('cloudinary');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const config = require('./config');
 
 const COMPLETE_STATUS = "complete";
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-const multipartMiddleware = multipart();
 
 cloudinary.config(config.cloudinary);
 
-app.post('/get-car-number', multipartMiddleware, (req, res) => {
-  cloudinary.v2.uploader.upload(
-    req.files.image.path,
-    {
-      ocr: "adv_ocr"
-    }, 
-    (error, result) => {
-        if(result.info.ocr.adv_ocr.status === COMPLETE_STATUS) {
-          res.json(result.info.ocr.adv_ocr.data[0].textAnnotations[0].description);
-        }
-    });
+app.get('/car-numbers', (req, res) => {
+  webcam.capture(
+    "numbers_picture",
+    { saveShots: false, callbackReturn: "base64"  },
+    (err, imgBase64) => {
+      cloudinary.v2.uploader.upload(
+      imgBase64,
+      {
+        ocr: "adv_ocr"
+      }, 
+      (error, result) => {
+          if(result.info.ocr.adv_ocr.status === COMPLETE_STATUS) {
+            res.json((result.info.ocr.adv_ocr.data[0].textAnnotations || [{}])[0].description);
+          }
+      });
+  });
+  
 });
 
 app.listen(config.port, () => console.log('Server with detecting car numbers is running'));
